8febac3f631c12a4b6f2bb6c05dc0e21c7857304
jw-lee96 <jungwon17.lee@navercorp.com>
2024-08-15 16:23:22 +0900

    create next project > install Full Calendar

---
+++ calendar-test/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Calendar from "./calendar";
+
+
+export default function Home() {
+  return (
+    <Calendar />
+  );
+}
