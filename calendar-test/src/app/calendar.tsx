b8e0d92b59449f6eaf0a88136547112dd187d067
jw-lee96 <jungwon17.lee@navercorp.com>
2024-08-15 16:28:19 +0900

    dateClick event test

--- calendar-test/src/app/calendar.tsx
+++ calendar-test/src/app/calendar.tsx
@@ -1,14 +1,21 @@
 "use client"
 
 import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction" // needed for dateClick
 
 
 export default function Calendar() {
+
+    const handleDateClick = (arg: any) => {
+        alert(JSON.stringify(arg));
+    }
+
   return (
     <FullCalendar
-      plugins={[ dayGridPlugin ]}
+      plugins={[ dayGridPlugin, interactionPlugin ]}
       initialView="dayGridMonth"
+      dateClick={handleDateClick}
     />
   );
 }
